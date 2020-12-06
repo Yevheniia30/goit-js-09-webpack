944395e3f1672c494ade5dfb615623475b492dbd
Evgeniya3010 <susskaya86@gmail.com>
2020-12-06 06:29:44 +0200

    practice

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-// let a = 2;
-// a = a << 2;
-// console.log(a); //8
+const sayWebpack = () => {
+  console.log("Webpack is amazing!");
+};
 
-// (function () {
-//   return typeof arguments; //obj
-// });
+const add = (a, b) => {
+  console.log(a + b);
+};
 
-// let b = 1;
-// let c = b++ + 1;
-// console.log(c); //2
-
-// function f(a, b, c) {}
-// const arr = [, , ,];
-// console.log(arr.length);
-// console.log(f.length); //3 3
+sayWebpack();
+add(5, 3);
